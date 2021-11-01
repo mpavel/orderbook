5322032ff3ee57e667e4a54590768a6db171ce8d
Pavel Mocan <pavelmocan@gmail.com>
2021-11-01 11:33:26 +0100

    Create basic UI for Order Book

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,22 @@ import './App.scss';
 import { OrderbookContainer } from '../orderbook/OrderbookContainer';
 
 function App() {
+    /*
+    const ws = new WebSocket('wss://www.cryptofacilities.com/ws/v1');
+    ws.onopen = (event) => {
+        const subscribeEvent = {
+            'event': 'subscribe',
+            'feed': 'book_ui_1',
+            'product_ids': ['PI_XBTUSD'],
+        };
+        ws.send(JSON.stringify(subscribeEvent));
+    }
+
+    ws.onmessage = (message) => {
+        console.log('Message received', message.data);
+    }
+     */
+
     return (
         <div className='App'>
             <OrderbookContainer />
--- src/orderbook/OrderbookContainer.tsx
+++ src/orderbook/OrderbookContainer.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import style from './Orderbook.module.scss';
+import './Orderbook.scss';
 
 interface Props {}
 
 export const OrderbookContainer: React.FC<Props> = ({}) => {
     return (
-        <div className={style.wrapper}>
-            <p>Orderbook Container</p>
+        <div className='wrapper'>
+            <div className='orderbook'>
+                <div className='title'>
+                    <div className='name'>Order Book</div>
+                    <div className='spread'>Spread: 17.0 (0.05%)</div>
+                </div>
+                <div className='body'>
+                    <div className='buySide'>
+                        <div className='header'>
+                            <div className='column'>Total</div>
+                            <div className='column'>Size</div>
+                            <div className='column'>Price</div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>1200</div>
+                            <div className='column'>1200</div>
+                            <div className='column price'>34,500.50</div>
+                            <div className='depth' style={{ width: '10%' }}></div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>1200</div>
+                            <div className='column'>1200</div>
+                            <div className='column price'>34,500.50</div>
+                            <div className='depth' style={{ width: '25%' }}></div>
+                        </div>
+                    </div>
+                    <div className='sellSide'>
+                        <div className='header'>
+                            <div className='column'>Total</div>
+                            <div className='column'>Size</div>
+                            <div className='column'>Price</div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>1200</div>
+                            <div className='column'>1200</div>
+                            <div className='column price'>34,500.50</div>
+                            <div className='depth' style={{ width: '10%' }}></div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>1200</div>
+                            <div className='column'>1200</div>
+                            <div className='column price'>34,500.50</div>
+                            <div className='depth' style={{ width: '25%' }}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
